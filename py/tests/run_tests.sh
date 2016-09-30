a697939f7c9880cf062040b37604d2e3cea1e015
Abe Handler <akh2103@gmail.com>
2016-09-30 13:45:12 -0400

    more pip stuff

--- py/tests/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-here="$(dirname $0)"
-
-echo "=== SMOKE TEST"
-PYTHONPATH=$here/.. python $here/smoketest.py 
-
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name = 'phrasemachine',
   packages = ['phrasemachine'], # this must be the same as the name above
   version = '1.0.0',
-  description = 'A random test lib',
+  description = 'Gets phrases from text using part-of-speech tags',
   author = 'slanglab, University of Massachusetts, Amherst',
   author_email = 'abram.handler@gmail.com',
   url = 'https://github.com/slanglab/phrasemachine', # use the URL to the github repo
-  download_url = 'TODO', # I'll explain this in a second
+  download_url = 'https://github.com/slanglab/phrasemachine/tarball', # I'll explain this in a second
   keywords = ['nlp', 'nlproc'], # arbitrary keywords
   classifiers = [],
 )

4659f2a03a51f987070d3141fcb842777d62b73c
Abe Handler <akh2103@gmail.com>
2016-09-30 14:26:05 -0400

    flailing towards pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'phrasemachine',
   packages = ['phrasemachine'], # this must be the same as the name above
-  version = '0.0.4',
+  version = '0.0.6',
   description = 'Gets phrases from text using part-of-speech tags',
   author = 'slanglab, University of Massachusetts, Amherst',
   author_email = 'abram.handler@gmail.com',
   url = 'https://github.com/slanglab/phrasemachine', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.0.4'
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.0.6',
   keywords = ['nlp', 'nlproc'], # arbitrary keywords
   classifiers = [],
 )

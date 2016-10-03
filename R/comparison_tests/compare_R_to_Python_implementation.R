cf71a66ae547dad915ee9cb3ab7a405f0397e6d3
Matt Denny <matthewjdenny@gmail.com>
2016-10-03 14:49:25 -0400

    more comparison output

--- R/comparison_tests/compare_R_to_Python_implementation.R
+++ R/comparison_tests/compare_R_to_Python_implementation.R
@@ -14,7 +14,7 @@ text <- paste0(text, collapse = " ")
 
 POS_tagged_R <- POS_tag_documents(text)
 
-phrases_R <- phrasemachine(text, return_tag_sequences = TRUE)
+phrases_R <- phrasemachine(text, return_tag_sequences = TRUE, maximum_ngram_length = 7)
 
 # load in python results
 library("rjson")
@@ -32,8 +32,101 @@ sum(unlist(phrases_Py$counts))
 length(phrases_R[[1]]$phrases)
 # so there are some differences, lets see why
 
+# first, find the different tokens
+R_tokens <- unique(tolower(POS_tagged_R[[1]]$tokens))
+Py_tokens <- unique(tolower(tokens_Py$tokens))
 
-# first, use Python POS taggings and tokens as imput for R phrase extractor
+for (i in 1:length(R_tokens)) {
+    if (length(which(Py_tokens == R_tokens[i])) != 1) {
+        print(R_tokens[i])
+    }
+}
+
+for (i in 1:length(Py_tokens)) {
+    if (length(which(R_tokens == Py_tokens[i])) != 1) {
+        print(Py_tokens[i])
+    }
+}
+
+# see if we remove these tokens if we get the same thing.
+POS_tagged_R[[1]]$tokens <- tolower(POS_tagged_R[[1]]$tokens)
+remove <- rep(0, length(POS_tagged_R[[1]]$tokens))
+for (i in 1:length(POS_tagged_R[[1]]$tokens)) {
+    if (length(which(Py_tokens == POS_tagged_R[[1]]$tokens[i])) == 0) {
+        remove[i] <- 1
+    }
+}
+
+POS_tagged_R[[1]]$tokens <- POS_tagged_R[[1]]$tokens[-which(remove == 1)]
+POS_tagged_R[[1]]$tags <- POS_tagged_R[[1]]$tags[-which(remove == 1)]
+
+# now from Python
+POS_tagged_from_Python <- POS_tagged_R
+POS_tagged_from_Python[[1]]$tokens <- tokens_Py$tokens
+POS_tagged_from_Python[[1]]$tags <- pos_tags_Py$pos
+remove <- rep(0, length(POS_tagged_R[[1]]$tokens))
+for (i in 1:length(POS_tagged_from_Python[[1]]$tokens)) {
+    if (length(which(R_tokens == POS_tagged_from_Python[[1]]$tokens[i])) == 0) {
+        remove[i] <- 1
+    }
+}
+
+POS_tagged_from_Python[[1]]$tokens <- POS_tagged_from_Python[[1]]$tokens[-which(remove == 1)]
+POS_tagged_from_Python[[1]]$tags <- POS_tagged_from_Python[[1]]$tags[-which(remove == 1)]
+
+# get phrases
+phrases_1 <- extract_phrases(POS_tagged_R, maximum_ngram_length = 7)
+phrases_2 <- extract_phrases(POS_tagged_from_Python, maximum_ngram_length = 7)
+
+# get counts of unique for both
+unique_phrases <- unique(tolower(phrases_1[[1]]))
+unique_counts <- rep(0,length(unique_phrases))
+for(i in 1:length(unique_phrases)) {
+    unique_counts[i] <- length(which(tolower(phrases_1[[1]]) == unique_phrases[i]))
+}
+
+Phrases_1 <- data.frame(term = unique_phrases[order(unique_phrases)],
+                        count = unique_counts[order(unique_phrases)],
+                        stringsAsFactors = FALSE)
+
+unique_phrases <- unique(tolower(phrases_2[[1]]))
+unique_counts <- rep(0,length(unique_phrases))
+for(i in 1:length(unique_phrases)) {
+    unique_counts[i] <- length(which(tolower(phrases_2[[1]]) == unique_phrases[i]))
+}
+
+Phrase_2 <- data.frame(term = unique_phrases[order(unique_phrases)],
+                        count = unique_counts[order(unique_phrases)],
+                        stringsAsFactors = FALSE)
+
+
+# now go through and find differences
+for(i in 1:nrow(Phrases_1)) {
+    in_py <- which(Phrase_2$term == Phrases_1$term[i])
+    if (length(in_py) == 1) {
+        if(Phrase_2$count[in_py] != Phrases_1$count[i]) {
+            cat(Phrases_1$term[i], "has count:",Phrases_1$count[i],"in R, but",Phrase_2$count[in_py],"in Python...\n")
+        }
+    } else {
+        cat(Phrases_1$term[i], "is not in Python output but occurs",Phrases_1$count[i],"times in R output...\n")
+    }
+}
+
+
+# no terms in python that are not in R
+for(i in 1:nrow(Phrase_2)) {
+    in_py <- which(Phrases_1$term == Phrase_2$term[i])
+    if (length(in_py) == 1) {
+        if(Phrases_1$count[in_py] != Phrase_2$count[i]) {
+            cat(Phrase_2$term[i],"has count:",Phrases_1$count[in_py],"in R, but",Phrase_2$count[i],"in Python...\n")
+        }
+    } else {
+        cat(Phrase_2$term[i], "is not in R output but occurs",Phrase_2$count[i],"times in Python output...\n")
+    }
+}
+
+
+# second, use Python POS taggings and tokens as imput for R phrase extractor
 POS_tagged_from_Python <- POS_tagged_R
 
 POS_tagged_from_Python[[1]]$tokens <- tokens_Py$tokens

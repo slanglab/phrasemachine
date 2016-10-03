6af7c07051d7d394eb5d52542a9553159d96e642
Matt Denny <matthewjdenny@gmail.com>
2016-10-02 22:04:34 -0400

    onload message

---
+++ R/phrasemachine/R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage("phrasemachine: Simple Phrase Extraction
+Version 0.2.1 created on 2016-10-02.
+copyright (c) 2016, Matthew J. Denny, Penn State University
+                    Abe Handler, UMass Amherst
+                    Hanna Wallach, Microsoft Research NYC
+                    Brendan O'Connor, UMass Amherst
+Type help('phrasemachine') to get started.
+Development website: http://slanglab.cs.umass.edu/phrases/")
+}

a640115d013d6d8b54c088aef664c6e1116c73b9
Matt Denny <matthewjdenny@gmail.com>
2016-10-03 10:40:04 -0400

    test basic example -- works from interactive shell

--- R/comparison_tests/ex.py
+++ R/comparison_tests/ex.py
@@ -1,3 +1,4 @@
-import phrasemachine
+# can only be run from the py directory, and only in interactive shell
+# import phrasemachine
 text = "Barack Obama supports expanding social security."
 print phrasemachine.get_phrases(text)

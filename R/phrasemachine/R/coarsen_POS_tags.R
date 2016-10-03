b742c6ff5cb57b4da38d548a56f0b27002d29f92
Matt Denny <matthewjdenny@gmail.com>
2016-10-02 20:47:07 -0400

    code up and test tag coarsener

---
+++ R/phrasemachine/R/coarsen_POS_tags.R
@@ -0,0 +1,23 @@
+#' @title Coarsen POS tags
+#' @description Coarsens PTB or Petrov/Gimpel tags into one of five categories:
+#' 'A' = ADJ, 'D' = DET, 'P' = ADP, 'N' = NOUN, 'O' = all else
+#'
+#' @param tag_vector A vector of POS tags.
+#' @return A vector of coarse tags.
+#' @export
+coarsen_POS_tags <- function(tag_vector) {
+    for (i in 1:length(tag_vector)) {
+        if (tag_vector[i] %in% c("JJ","JJR","JJS","CoarseADJ", "CD", "CoarseNUM")) {
+            tag_vector[i] <- "A"
+        } else if (tag_vector[i] %in% c("DT","CoarseDET")) {
+            tag_vector[i] <- "D"
+        } else if (tag_vector[i] %in% c("IN", "TO", "CoarseADP")) {
+            tag_vector[i] <- "P"
+        } else if (tag_vector[i] %in% c("NN", "NNS", "NNP", "NNPS", "FW", "CoarseNOUN")) {
+            tag_vector[i] <- "N"
+        } else {
+            tag_vector[i] <- "O"
+        }
+    }
+    return(tag_vector)
+}
--- R/phrasemachine/tests/testthat/test_POS_tag_documents.R
+++ R/phrasemachine/tests/testthat/test_POS_tag_documents.R
@@ -1,5 +1,5 @@
-context("Factorial Preprocessing")
-test_that("Small example works", {
+context("POS Tagger")
+test_that("See if tagging five documents works, and if coarsening works", {
     # load data
     corp <- quanteda::corpus(quanteda::inaugTexts)
     documents <- quanteda::texts(corp)[1:5]
@@ -7,5 +7,6 @@ test_that("Small example works", {
     # run tagger
     tagged_documents <- POS_tag_documents(documents)
 
-
+    # try coarsening one set of tags
+    coarse_tags <- coarsen_POS_tags(tagged_documents[[1]]$tags)
 })

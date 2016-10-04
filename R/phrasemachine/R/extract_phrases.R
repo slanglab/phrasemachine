352f3c2ff8d33ab5d0ebb2ec6af2fd4815c25d64
Matt Denny <matthewjdenny@gmail.com>
2016-10-04 14:08:09 -0400

    add option to exclude unigrams and deal with NULL return

--- R/phrasemachine/R/extract_phrases.R
+++ R/phrasemachine/R/extract_phrases.R
@@ -5,7 +5,7 @@
 #' @param POS_tagged_documents A list object of the form produced by the
 #' `POS_tag_documents()` function, with either Penn TreeBank or Petrov/Gimpel
 #' style tags.
-#' @param regex The regualr expression used to find phrases. Defaults to
+#' @param regex The regular expression used to find phrases. Defaults to
 #' "(A|N)*N(PD*(A|N)*N)*", as in Handler et al. 2016.
 #' @param maximum_ngram_length The maximum length phrases returned. Defaults to
 #' 8. Increasing this number can greatly increase runtime.
@@ -15,13 +15,16 @@
 #' string.
 #' @param return_tag_sequences Logical indicating whether tag sequences should
 #' be returned along with phrases. Defaults to FALSE.
+#' @param exclude_unigrams Option to exclude all unigrams from phrases returned.
+#' Defaults to FALSE.
 #' @return A list object.
 #' @export
 extract_phrases <- function(POS_tagged_documents,
                             regex = "(A|N)*N(PD*(A|N)*N)*",
                             maximum_ngram_length = 8,
                             return_phrase_vectors = TRUE,
-                            return_tag_sequences = FALSE) {
+                            return_tag_sequences = FALSE,
+                            exclude_unigrams = FALSE) {
     # determine the return type
     if (return_phrase_vectors) {
         to_return <- vector(mode = "list", length = length(POS_tagged_documents))
@@ -35,42 +38,47 @@ extract_phrases <- function(POS_tagged_documents,
         # get the spans
         spans <- extract_ngram_filter(pos_tags = POS_tagged_documents[[i]]$tags,
                                       regex = regex,
-                                      maximum_ngram_length = maximum_ngram_length)
-        # allocate phrases vector
-        phrases <- rep("",nrow(spans))
+                                      maximum_ngram_length = maximum_ngram_length,
+                                      exclude_unigrams = exclude_unigrams)
 
-        # if we are returning tags, tehn coarsen them to put in output
-        if (return_tag_sequences) {
-            pos_tags <- coarsen_POS_tags(POS_tagged_documents[[i]]$tags)
-            tagseqs <- rep("",nrow(spans))
-        }
+        # only get phrases if we found spans
+        if (!is.null(spans)) {
+            # allocate phrases vector
+            phrases <- rep("",nrow(spans))
 
-
-        for (j in 1:length(phrases)) {
-            #populate phrases vector
-            phrases[j] <- paste0(
-                POS_tagged_documents[[i]]$tokens[spans[j,1]:spans[j,2]],
-                collapse = "_")
+            # if we are returning tags, tehn coarsen them to put in output
             if (return_tag_sequences) {
-                tagseqs[j] <- paste0(
-                    pos_tags[spans[j,1]:spans[j,2]],
-                    collapse = "")
+                pos_tags <- coarsen_POS_tags(POS_tagged_documents[[i]]$tags)
+                tagseqs <- rep("",nrow(spans))
             }
-        }
 
-        if (return_phrase_vectors) {
-            if (return_tag_sequences) {
-                to_return[[i]] <- list(phrases = phrases,
-                                       tags = tagseqs)
-            } else {
-                to_return[[i]] <- phrases
+
+            for (j in 1:length(phrases)) {
+                #populate phrases vector
+                phrases[j] <- paste0(
+                    POS_tagged_documents[[i]]$tokens[spans[j,1]:spans[j,2]],
+                    collapse = "_")
+                if (return_tag_sequences) {
+                    tagseqs[j] <- paste0(
+                        pos_tags[spans[j,1]:spans[j,2]],
+                        collapse = "")
+                }
             }
-        } else {
-            if (return_tag_sequences) {
-                to_return2[[i]] <- list(phrases =  paste0(phrases,collapse = " "),
-                                        tags =  paste0(tagseqs,collapse = " "))
+
+            if (return_phrase_vectors) {
+                if (return_tag_sequences) {
+                    to_return[[i]] <- list(phrases = phrases,
+                                           tags = tagseqs)
+                } else {
+                    to_return[[i]] <- phrases
+                }
             } else {
-                to_return[i] <- paste0(phrases,collapse = " ")
+                if (return_tag_sequences) {
+                    to_return2[[i]] <- list(phrases =  paste0(phrases,collapse = " "),
+                                            tags =  paste0(tagseqs,collapse = " "))
+                } else {
+                    to_return[i] <- paste0(phrases,collapse = " ")
+                }
             }
         }
     }

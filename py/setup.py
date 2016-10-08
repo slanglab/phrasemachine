d6ec84b8316b0279d54f15bb00a041cd7ae0b6bf
Abe Handler <akh2103@gmail.com>
2016-10-08 09:14:38 -0400

    data files change

--- py/setup.py
+++ py/setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'phrasemachine',
   packages = ['phrasemachine'], # this must be the same as the name above
-  version = '0.0.19',
+  version = '0.0.20',
   description = 'Gets phrases from text using part-of-speech tags',
   author = 'slanglab, University of Massachusetts, Amherst',
   author_email = 'abram.handler@gmail.com',
   url = 'https://github.com/slanglab/phrasemachine', # use the URL to the github repo
-  download_url = 'https://github.com/slanglab/phrasemachine/tarball/0.0.19',
+  download_url = 'https://github.com/slanglab/phrasemachine/tarball/0.0.20',
   keywords = ['nlp', 'nlproc'], # arbitrary keywords
   install_requires=['nltk'],
   classifiers = [],
-  package_data={'phrasemachine': ['data/*.pickle']},
+  data_files={'pickled': ['data/*.pickle']},
 )

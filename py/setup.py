1fa646ac9f97ef71a8ad473e66eb2d1e7f083d28
Abe Handler <akh2103@gmail.com>
2016-10-08 08:18:50 -0400

    ?

--- py/setup.py
+++ py/setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'phrasemachine',
   packages = ['phrasemachine'], # this must be the same as the name above
-  version = '0.0.11',
+  version = '0.0.13',
   description = 'Gets phrases from text using part-of-speech tags',
   author = 'slanglab, University of Massachusetts, Amherst',
   author_email = 'abram.handler@gmail.com',
   url = 'https://github.com/slanglab/phrasemachine', # use the URL to the github repo
-  download_url = 'https://github.com/slanglab/phrasemachine/tarball/0.0.11',
+  download_url = 'https://github.com/slanglab/phrasemachine/tarball/0.0.13',
   keywords = ['nlp', 'nlproc'], # arbitrary keywords
   install_requires=['nltk'],
   classifiers = [],

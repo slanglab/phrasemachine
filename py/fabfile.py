768aa6eda922da7a85969bc145244428c9a76087
Abe Handler <akh2103@gmail.com>
2016-10-11 09:58:29 -0400

    more automation todos

--- py/fabfile.py
+++ py/fabfile.py
@@ -1,5 +1,10 @@
 from fabric.api import local
 
+# TODO: automate this part
+# http://peterdowns.com/posts/first-time-with-pypi.html
+# git tag 0.0.23 -m 0.0.23
+# git push --tags origin master
+
 def deploy():
     local("git push --tags origin master")
     local("python setup.py sdist upload -r pypi")

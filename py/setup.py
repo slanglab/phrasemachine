ad9c20608062900ae7fcc217bb64c7d007d1dd3e
Abe Handler <akh2103@gmail.com>
2016-10-15 21:55:49 -0400

    pip still work?

--- py/setup.py
+++ py/setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'phrasemachine',
   packages = ['phrasemachine', 'phrasemachine.data'], # this must be the same as the name above
-  version = '0.0.23',
+  version = '0.0.24',
   description = 'Gets phrases from text using part-of-speech tags',
   author = 'slanglab, University of Massachusetts, Amherst',
   author_email = 'abram.handler@gmail.com',
   url = 'https://github.com/slanglab/phrasemachine', # use the URL to the github repo
-  download_url = 'https://github.com/slanglab/phrasemachine/tarball/0.0.23',
+  download_url = 'https://github.com/slanglab/phrasemachine/tarball/0.0.24',
   keywords = ['nlp', 'nlproc'], # arbitrary keywords
   install_requires=['nltk'],
   classifiers = [],

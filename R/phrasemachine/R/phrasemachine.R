6695673c0c991f798826461786d0323529712ec4
Matt Denny <matthewjdenny@gmail.com>
2016-10-21 16:16:01 -0400

    min length

--- R/phrasemachine/R/phrasemachine.R
+++ R/phrasemachine/R/phrasemachine.R
@@ -7,14 +7,15 @@
 #' "(A|N)*N(PD*(A|N)*N)*", as in Handler et al. 2016.
 #' @param maximum_ngram_length The maximum length phrases returned. Defaults to
 #' 8. Increasing this number can greatly increase runtime.
+#' @param minimum_ngram_length The minimum length phrases returned. Defaults to
+#' 2. Can be increased to remove shorter phrases, or decreased to include
+#' unigrams.
 #' @param return_phrase_vectors Logical indicating whether a list of phrase
 #' vectors (with each entry contain a vector of phrases in one document) should
 #' be returned, or whether phrases should combined into a single space separated
 #' string.
 #' @param return_tag_sequences Logical indicating whether tag sequences should
 #' be returned along with phrases. Defaults to FALSE.
-#' @param exclude_unigrams Option to exclude all unigrams from phrases returned.
-#' Defaults to FALSE.
 #' @examples
 #' phrasemachine("Hello there my red good cat.")
 #' @return A list object.
@@ -22,9 +23,9 @@
 phrasemachine <- function(documents,
                           regex = "(A|N)*N(PD*(A|N)*N)*",
                           maximum_ngram_length = 8,
+                          minimum_ngram_length = 2,
                           return_phrase_vectors = TRUE,
-                          return_tag_sequences = FALSE,
-                          exclude_unigrams = FALSE) {
+                          return_tag_sequences = FALSE) {
 
     # tag documents
     tagged_documents <- POS_tag_documents(documents)
@@ -33,9 +34,9 @@ phrasemachine <- function(documents,
     phrases <- extract_phrases(tagged_documents,
                                regex = regex,
                                maximum_ngram_length = maximum_ngram_length,
+                               minimum_ngram_length = minimum_ngram_length,
                                return_phrase_vectors = return_phrase_vectors,
-                               return_tag_sequences = return_tag_sequences,
-                               exclude_unigrams = exclude_unigrams)
+                               return_tag_sequences = return_tag_sequences)
 
     return(phrases)
 }

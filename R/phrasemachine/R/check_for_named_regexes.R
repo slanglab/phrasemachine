44dc5565bd0ac7030348252381565bb4ad443b3c
Matt Denny <matthewjdenny@gmail.com>
2017-02-20 23:22:00 -0500

    better code documentation

--- R/phrasemachine/R/check_for_named_regexes.R
+++ R/phrasemachine/R/check_for_named_regexes.R
@@ -1,31 +1,39 @@
 check_for_named_regexes <- function(regexes) {
-    # if we are not doing anything, then
+    # initialize output vector
     output <- NULL
 
     # loop over the regular expressions and replace special names with the
-    # appropriate regular expression.
+    # appropriate regular expression, or just the supplied regular expression.
     for (i in 1:length(regexes)) {
         if (regexes[i] == "SimpleNP") {
+            # allow the user to just put in "SimpleNP" for the regex, which gets
+            # replaced with the correct regex.
             output <- c(output, "(A|N)*N(PD*(A|N)*N)*")
         } else if ((regexes[i] == "PhrasesNoCoord")){
+            # thhis gets NP + VP (but with no coordination).
             output <- c(output,
-                        "(A|N)*N(P+D*(A|N)*N)*",
-                        "((A|N)*N(P+D*(A|N)*N)*P*(M|V)*V(M|V)*|(M|V)*V(M|V)*D*(A|N)*N(P+D*(A|N)*N)*|(M|V)*V(M|V)*(P+D*(A|N)*N)+|(A|N)*N(P+D*(A|N)*N)*P*((M|V)*V(M|V)*D*(A|N)*N(P+D*(A|N)*N)*|(M|V)*V(M|V)*(P+D*(A|N)*N)+))")
+                        "(A|N)*N(P+D*(A|N)*N)*", # Just NP
+                        "((A|N)*N(P+D*(A|N)*N)*P*(M|V)*V(M|V)*|(M|V)*V(M|V)*D*(A|N)*N(P+D*(A|N)*N)*|(M|V)*V(M|V)*(P+D*(A|N)*N)+|(A|N)*N(P+D*(A|N)*N)*P*((M|V)*V(M|V)*D*(A|N)*N(P+D*(A|N)*N)*|(M|V)*V(M|V)*(P+D*(A|N)*N)+))") # Verb-Arg
         } else if ((regexes[i] == "Phrases")){
-            # now with coordination
+            # now get NP + VP with coordination
             output <- c(output,
-                        "((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)",
-                        "(((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)(P(CP)*)*(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*|(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*(D(CD)*)*((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)|(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)+|((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)(P(CP)*)*((M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*(D(CD)*)*((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)|(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)+))")
+                        "((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)", # NP
+                        "(((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)(P(CP)*)*(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*|(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*(D(CD)*)*((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)|(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)+|((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)(P(CP)*)*((M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*(D(CD)*)*((A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*(C(D(CD)*)*(A(CA)*|N)*N((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)*)*)|(M(CM)*|V)*V(M(CM)*|V)*(C(M(CM)*|V)*V(M(CM)*|V)*)*((P(CP)*)+(D(CD)*)*(A(CA)*|N)*N)+))") # VP
         } else {
-            # if it is just a regex, then just append it to the vector
-            output <- c(output, regexes)
+            # if the current rety in regexes is just a regex, then just append it
+            # to the output vector
+            output <- c(output, regexes[i])
         }
     }
 
+    # return everything
     return(output)
 }
 
-# verby grammar (no coordination)
+# below are the methods by which I generated the above regexes. I decided to
+# just hardcode them to minimize the possibility of weird OS specific issues.
+
+# NP + verby grammar (no coordination)
 # BaseNP <- "(A|N)*N"
 # PP <- paste("P+D*",BaseNP, sep = "")
 # NP <- paste(BaseNP,"(",PP,")*",sep = "")
@@ -37,7 +45,7 @@ check_for_named_regexes <- function(regexes) {
 # VerbArg <- paste("(",SV,"|",VO,"|",VPP,"|",NPVP,")",sep ="")
 # reg <- c(NP,VerbArg)
 
-# # now try with the verby grammar (with coordination)
+# NP + Very grammar (with coordination)
 # A <- "A(CA)*"
 # D <- "(D(CD)*)"
 # M <- "M(CM)*"
@@ -53,4 +61,4 @@ check_for_named_regexes <- function(regexes) {
 # VPP <- paste(VG,"(",PP,")+", sep = "")
 # NPVP <- paste(NP,P,"*","(",VO,"|",VPP,")",sep = "")
 # VerbArg <- paste("(",SV,"|",VO,"|",VPP,"|",NPVP,")",sep ="")
-# reg <- c(VerbArg,NP)
+# reg <- c(NP,VerbArg)

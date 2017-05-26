ce8b0c45b9a3ce93d8b40eb21472f063a916b184
Matt Denny <matthewjdenny@gmail.com>
2017-05-25 23:38:57 -0400

    version bump for new release

--- R/phrasemachine/R/zzz.R
+++ R/phrasemachine/R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("phrasemachine: Simple Phrase Extraction
-Version 1.1.0 created on 2017-02-19.
+Version 1.1.1 created on 2017-05-25.
 copyright (c) 2016, Matthew J. Denny, Abram Handler, Brendan O'Connor.
 Type help('phrasemachine') or
 vignette('getting_started_with_phrasemachine') to get started.

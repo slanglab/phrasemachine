dc9013ca4fbe715a3a31da9f0fa50a61449a582b
Abe Handler <abram.handler@gmail.com>
2019-06-26 09:37:39 -0400

    pm

--- py/phrasemachine/phrasemachine.py
+++ py/phrasemachine/phrasemachine.py
@@ -26,10 +26,10 @@ if sys.version_info[0] >= 3:
 # "Coarse*" indicates petrov/gimpel
 # Grammar change from the FST version: can't repeat NUM in both adj and noun.
 coarsemap = {
-'A': "JJ JJR JJS CoarseADJ CD CoarseNUM A".split(),
-'D': "DT CoarseDET D".split(),
-'P': "IN TO CoarseADP P".split(),
-'N': "NN NNS NNP NNPS FW CoarseNOUN N".split(),
+'A': "JJ JJR JJS CoarseADJ CD CoarseNUM A ADJ".split(),
+'D': "DT CoarseDET D DET".split(),
+'P': "IN TO CoarseADP P ADP".split(),
+'N': "NN NNS NNP NNPS FW CoarseNOUN N NOUN PROPN".split(),
 # all other tags get O
 }
 
